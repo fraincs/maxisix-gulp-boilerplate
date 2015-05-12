641ff45099ff3f98eb654386ff98dc7d59f23bdb
Francis Thibault <franci@agencehoffman.com>
2015-05-12 13:22:57 -0400

    modifs folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -10,30 +10,30 @@ var gulp = require('gulp'),
 
 
     /* STYLES DEPENDENCIES */
-	stylus = require('gulp-stylus'),
-	sourcemaps = require('gulp-sourcemaps'),
-	rupture = require('rupture'),
-	autoprefixer = require('gulp-autoprefixer'),
-	cmq = require('gulp-combine-media-queries'),
+    stylus = require('gulp-stylus'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rupture = require('rupture'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cmq = require('gulp-combine-media-queries'),
 
 
     /* JS DEPENDENCIES */
     jshint = require('gulp-jshint'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	stylish = require('jshint-stylish'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    stylish = require('jshint-stylish'),
 
 
     /* IMAGES MINIFICATION DEPENDENCIES */
     imagemin = require('gulp-imagemin'),
-	pngquant = require('imagemin-pngquant'),
+    pngquant = require('imagemin-pngquant'),
 
 
     /* SVG SPRITES DEPENDENCIES */
     svgstore = require('gulp-svgstore'),
-	svgmin = require('gulp-svgmin'),
-	rename = require('gulp-rename'),
-	cheerio = require('gulp-cheerio');
+    svgmin = require('gulp-svgmin'),
+    rename = require('gulp-rename'),
+    cheerio = require('gulp-cheerio');
 
 
 
@@ -45,24 +45,25 @@ FILE DESTINATIONS (RELATIVE TO ASSSETS FOLDER)
 
 var root_paths = {
 
-	assets : './assets/'
+    assets : './assets/'
+    src : './src/'
 
 };
 
 var target = {
 
-	main_stylus_src : root_paths.assets + 'stylus/styles.styl',
-    stylus_src : root_paths.assets + 'stylus/**/*.styl',               // all stylus files
-    css_dest : root_paths.assets + 'css',                         // where to put minified css
+    main_stylus_src : root_paths.src + 'stylus/styles.styl',
+    stylus_src : root_paths.src + 'stylus/**/*.styl',               // all stylus files
+    css_dest : root_paths.assets + 'css',                           // where to put minified css
 
-    js_src : root_paths.assets + 'js/*.js',						  // all js files
-    js_dest : root_paths.assets + 'js/min',                       // where to put minified js
+    js_src : root_paths.src + 'js/*.js',                            // all js files
+    js_dest : root_paths.assets + 'js/min',                         // where to put minified js
 
-	img_src : root_paths.assets + 'images/*.{png,jpg,gif,svg}',		  // all img files
-	img_dest : root_paths.assets + 'images/min',				  // where to put minified img
+    img_src : root_paths.assets + 'images/*.{png,jpg,gif,svg}',       // all img files
+    img_dest : root_paths.assets + 'images/min',                     // where to put minified img
 
-	svg_src : root_paths.assets + 'images/svg/*.svg',
-	svg_dest : root_paths.assets
+    svg_src : root_paths.assets + 'images/svg/*.svg',
+    svg_dest : root_paths.assets
 
 };
 
@@ -75,7 +76,7 @@ AUTOPREFIXER CONFIG
 *******************************************************************************/
 
 var AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
+    'ie >= 9',
     'ie_mob >= 10',
     'ff >= 30',
     'chrome >= 34',
@@ -95,22 +96,22 @@ STYLUS TASK
 *******************************************************************************/
 
 gulp.task('styles', function() {
-	return gulp.src(target.main_stylus_src)
-		.pipe(plumber())
-		.pipe(stylus({
-			sourcemap: {
-				inline: true,
-				sourceRoot: '',
-				basePath: 'css'
-			},
-			use:[rupture()],
-		}))
-		.pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-		.pipe(cmq({
-			log: true
-		}))
-		.pipe(gulp.dest(target.css_dest))
-		.pipe(notify('Styles task completed'));
+    return gulp.src(target.main_stylus_src)
+        .pipe(plumber())
+        .pipe(stylus({
+            sourcemap: {
+                inline: true,
+                sourceRoot: '',
+                basePath: 'css'
+            },
+            use:[rupture()],
+        }))
+        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+        .pipe(cmq({
+            log: true
+        }))
+        .pipe(gulp.dest(target.css_dest))
+        .pipe(notify('Styles task completed'));
 });
 
 
@@ -122,14 +123,14 @@ JS TASK
 *******************************************************************************/
 
 gulp.task('scripts', function() {
-	return gulp.src(target.js_src)
-		.pipe(plumber())
-		.pipe(jshint())
-		.pipe(jshint.reporter(stylish))
-		.pipe(concat('scripts.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(target.js_dest))
-		.pipe(notify('Scripts task completed'));
+    return gulp.src(target.js_src)
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(target.js_dest))
+        .pipe(notify('Scripts task completed'));
 });
 
 
@@ -141,14 +142,14 @@ IMAGES TASK
 *******************************************************************************/
 
 gulp.task('images', function() {
-	return gulp.src(target.img_src)
-		.pipe(plumber())
-		.pipe(imagemin({
+    return gulp.src(target.img_src)
+        .pipe(plumber())
+        .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-		.pipe(gulp.dest(target.img_dest));
+        .pipe(gulp.dest(target.img_dest));
 });
 
 
@@ -160,7 +161,7 @@ SVGSTORE TASK
 *******************************************************************************/
 
 gulp.task('svgstore', function() {
-	return gulp.src(target.svg_src)
+    return gulp.src(target.svg_src)
         .pipe(rename({ prefix: 'icon-' }))
         .pipe(svgmin())
         .pipe(svgstore({ inlineSvg: true }))
@@ -192,7 +193,8 @@ WATCH TASK
 
 gulp.task('watch', function() {
 
-	gulp.watch(target.stylus_src, ['styles']);		// Watch .styl files
-	gulp.watch(target.js_src, ['scripts']);			// Watch .js files
+    gulp.watch(target.stylus_src, ['styles']);       // Watch .styl files
+    gulp.watch(target.img_src, ['images']);         // Watch images files
+    gulp.watch(target.js_src, ['scripts']);        // Watch .js files
 
 });
